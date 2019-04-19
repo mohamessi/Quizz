6404e573e362f39afba7b39e0651809fbe9acb31
mohamessi <mo-2010@live.fr>
2019-04-19 23:44:45 +0200

    adding of components home and fast adding of service quizz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FastComponent } from './pages/fast/fast.component';
+import { QuizzComponent } from './services/quizz/quizz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FastComponent,
+    QuizzComponent
   ],
   imports: [
     BrowserModule,
