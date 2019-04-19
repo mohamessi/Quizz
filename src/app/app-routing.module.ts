b0ffc0b55ca84fa6fefae3ebeca3e80bca4f8a32
mohamessi <mo-2010@live.fr>
2019-04-20 00:46:38 +0200

    Home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FastComponent } from './pages/fast/fast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component : HomeComponent},
+  { path: 'fast', component: FastComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
