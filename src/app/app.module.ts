a0b058831407749827268e0df1eec5b7077e1c15
mohamessi <mo-2010@live.fr>
2019-04-21 18:26:20 +0200

    add of new directive : Question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FastComponent } from './pages/fast/fast.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { QuestionComponent } from './directives/question/question.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FastComponent
+    FastComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
