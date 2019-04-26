37d591b6e8cb171826105aac0f7145dfcce0d677
mohamessi <mo-2010@live.fr>
2019-04-26 21:37:45 +0200

    push test

---
+++ src/app/pages/personalized/personalized.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { QuizzService } from 'src/app/services/quizz.service';
+
+@Component({
+  selector: 'app-personalized',
+  templateUrl: './personalized.component.html',
+  styleUrls: ['./personalized.component.scss']
+})
+export class PersonalizedComponent implements OnInit {
+
+  constructor(private quizzService: QuizzService) { }
+
+  ngOnInit() {
+  }
+
+  generateQuizz()
+  {
+  
+  }
+
+  displayElemForm()
+  {}
+
+}
